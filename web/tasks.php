d7df982e1d2705369dcdd7921baef17cb55861f2
金子陽介 <kanekoyousuke@kanekoyousuke-no-MacBook-Air.local>
2017-03-21 06:47:17 +0900

    table change

--- web/tasks.php
+++ web/tasks.php
@@ -4,13 +4,13 @@
 	$data = json_decode(file_get_contents('php://input'));
 
 	if ($_SERVER['REQUEST_METHOD'] == "GET"){
-		$statement = $db->query('SELECT * FROM tasks');
+		$statement = $db->query('SELECT * FROM teams');
 		$statement->setFetchMode(PDO::FETCH_ASSOC);
 		echo json_encode($statement->fetchAll());
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] == "POST"){
-		$sql = "INSERT INTO tasks (title) values (:title)";
+		$sql = "INSERT INTO teams (title) values (:title)";
 		$query = $db->prepare($sql);
 		$query->execute(array(":title"=>$data->title));
 		$result['id'] = $db->lastInsertId();
@@ -18,13 +18,13 @@
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] == "PUT"){
-		$sql = "UPDATE tasks SET done = :done WHERE id = :id";
+		$sql = "UPDATE teams SET done = :done WHERE id = :id";
 		$query = $db->prepare($sql);
 		$query->execute(array(":done"=>$data->done, ":id"=>$data->id));
 	}
 	
 	if ($_SERVER['REQUEST_METHOD'] == "DELETE"){
-		$sql = "DELETE FROM tasks WHERE id = :id";
+		$sql = "DELETE FROM teams WHERE id = :id";
 		$query = $db->prepare($sql);
 		$query->execute(array(":id"=>$_GET['id']));
 	}
